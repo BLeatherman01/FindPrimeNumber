a1e4adc549a207c1f3732197e1eb47bbd3c4bd32
brandon <bleatherman01@gmail.com>
2022-10-24 20:24:52 -0400

    First commit

---
+++ PrimeNumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindPrimeTest
+{
+     class PrimeNumber
+    {
+        public int GetPrime()
+        {
+            Console.WriteLine("Please enter a positive integer");
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+
+            //negative check
+            if (num <= 0)
+            {
+                GetPrime();
+            }
+            int primeCount = 0;
+            int number = 2; 
+            while (true)
+            {
+                bool isPrime = true;
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    if (number % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    primeCount++;
+                    if (primeCount == num)
+                    {
+                        Console.WriteLine($"{num}th Prime Number is {number}");
+                        return number;
+                    }
+                }
+                number++;
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace FindPrimeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimeNumber gpn = new PrimeNumber();
+
+            bool askAgain = true;
+            while (askAgain)
+            {
+                try
+                {
+                    gpn.GetPrime();
+                }
+               
+                catch(FormatException e) 
+                {
+                    gpn.GetPrime();
+                }
+
+                askAgain = GoAgain();
+            }
+        }
+
+        public static bool GoAgain()
+        {
+            Console.WriteLine("\nDo you want to find another prime number? Please enter Yes or No");
+            string userInput = Console.ReadLine().ToLower().Trim();
+
+            if (userInput == "yes" || userInput == "y")
+            {
+
+                return true;
+            }
+            else if (userInput == "no" || userInput == "n")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Please enter Yes or No");
+                return GoAgain();
+            }
+        }
+    }
+}
